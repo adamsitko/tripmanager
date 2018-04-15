4e2b15c986f457ac97592e2014c236e572e8e80e
unknown <adamsitko1@op.pl>
2018-04-15 10:53:08 +0200

    Trip added

--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,5 +1,16 @@
 package pl.edu.agh.mwo;
- 
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TripManagerTest {
+	
+	@Test
+	public void testIfTripExist() {
+	Trip t = new Trip();
+	assertNotNull(t);
+	}
+	
 	
 }
